e23e95e4bde17c81e64ac03d45090ce2e231167c
Gokhan9 <gcancomert@hotmail.com>
2026-06-02 17:12:41 +0300

    usage logs tablosu create ettim.

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -4,10 +4,23 @@ CREATE TABLE users (
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE api_keys(
+CREATE TABLE api_keys (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     key_value VARCHAR(255) UNIQUE NOT NULL,
     is_active BOOLEAN DEFAULT true,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE usage_logs (
+    id SERIAL PRIMARY KEY,
+    api_key_id INTEGER NOT NULL REFERENCES api_keys(id),
+    provider VARCHAR(50) NOT NULL,
+    model VARCHAR(50) NOT NULL,
+    prompt_tokens INTEGER DEFAULT 0, -- API isteğinde kullanılan prompt token sayısı
+    completion_tokens INTEGER DEFAULT 0, -- API isteğinde kullanılan tamamlanan token sayısı
+    total_tokens INTEGER DEFAULT 0, -- API isteğinde kullanılan toplam token sayısı
+    latency_ms INTEGER, -- İstek işleme süresi(ms cinsinden)
+    status_code INTEGER, -- API isteğinin HTTP durum kodu
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
