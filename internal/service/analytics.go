38528281172a939ad01a79e28c555f077a6c5ae5
Gokhan9 <gcancomert@hotmail.com>
2026-06-02 20:24:56 +0300

    record ve worker eklendi.

---
+++ internal/service/analytics.go
@@ -0,0 +1,47 @@
+package service
+
+import (
+	"interflow/internal/analytics"
+	"interflow/internal/database"
+	"log"
+)
+
+type AnalyticsService struct {
+	repo        *database.Queries         // AnalyticsService, veritabanı sorgularını gerçekleştirmek için database.Queries türünde bir repo alanına sahiptir.
+	eventChan   chan analytics.UsageEvent // API eventlarını asenkron olarak işlemek için bir kanal (channel) kullanır.
+	WorkerCount int                       // Olayları işlemek için kaç tane işçi (worker) kullanılacağını belirten bir alan.
+}
+
+func NewService(repo *database.Queries, workerCount int) *AnalyticsService {
+	s := &AnalyticsService{
+		repo:        repo,                                  // Veritabanı sorguları için repo atanır
+		eventChan:   make(chan analytics.UsageEvent, 1000), // Olayları asenkron olarak işlemek için bir kanal oluşturulur (1000 kapasite sınırı)
+		WorkerCount: workerCount,                           // İşçi sayısı atanır
+	}
+
+	// ? Belirtilen sayıda işçi (worker) başlatılır. Her işçi, eventChan kanalından gelen UsageEvent'leri dinler ve işleme alır.
+	for i := 0; i < workerCount; i++ {
+		go s.worker()
+	}
+
+	return s
+
+}
+
+// "RECORD" bir event'i queue'ya atar.(Bloklamaz)
+func (s *AnalyticsService) Record(event analytics.UsageEvent) {
+	select {
+	case s.eventChan <- event:
+		// Queue'ya başarıyla ekler.
+	default:
+		// Queue doluysa log düşebilir veya veri drop edilir. (HIZ'I Önceliklendiriyoruz.)
+		log.Println("Analytics queue full, drop event.")
+	}
+}
+
+func (s *AnalyticsService) worker() {
+	for event := range s.eventChan {
+		// Burada DB'ye kayıt işlemi yapılacak (s.repo.CreateUsageLog(...))
+		// Bir hata olursa burada yakalanır, ana isteği etkilemez.
+	}
+}
