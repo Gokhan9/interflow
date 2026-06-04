6d1d715be4f3351cda299eda0a7ad20da46d20f7
Gokhan9 <gcancomert@hotmail.com>
2026-06-04 12:27:23 +0300

    AnalyticsService 5 worker ile başlatıldı, HTTP server yapılandırıldı ve ayrı bir goroutine içerisinde çalışacak şekilde düzenlendi. SIGINT ve SIGTERM sinyalleri için kapatma mekanizması eklendi. Graceful shutdown süreci ile birlikte yeni HTTP istekleri durduruluyor, 5–10 saniyelik bir kapanış penceresi tanımlanarak AnalyticsService’in queue’da bekleyen verileri veritabanına yazması sağlanıyor ve ardından server güvenli şekilde sonlandırılıyor.

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -1,26 +1,33 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"interflow/internal/cache"
 	"interflow/internal/config"
 	"interflow/internal/database"
 	"interflow/internal/middleware"
 	"interflow/internal/repository"
+	"interflow/internal/service"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
-	// ! Config Yükleme
+	//! Config Yükleme
 	cfg, err := config.LoadConfig()
 	if err != nil {
 		log.Fatalf("Error. Config is not Loaded: %v", err)
 	}
 
-	// ! Database Bağlantısı (Connection dosyasına taşıdım.)
+	//! Database Bağlantısı (Connection dosyasına taşıdım.)
 	err = repository.InitDB(cfg.DBURL)
 	if err != nil {
 		log.Fatalf("Database connection error: %v", err)
@@ -28,7 +35,10 @@ func main() {
 	log.Println("Database connection successful - ✅")
 	queries := database.New(repository.DB) // Repository katmanını başlatıyoruz. Bu, veritabanı işlemlerini gerçekleştirmek için kullanılacak sorguları içerir. Repository.New fonksiyonu, pgxpool.Pool türünde bir veritabanı bağlantısı alır ve bu bağlantıyı kullanarak sorguları hazırlar. Bu sayede, uygulamanın diğer bölümlerinde veritabanı işlemleri için bu sorguları kullanabiliriz.
 
-	// ! Redis Bağlantısı
+	// ? 1- Analytics Service'i 5 WORKER ile başlatıyoruz..
+	analyticsService := service.NewService(queries, 5)
+
+	//! Redis Bağlantısı
 	err = cache.InitRedis(cfg.RedisURL)
 	if err != nil {
 		log.Fatalf("Redis connection error: %v", err)
@@ -37,19 +47,46 @@ func main() {
 
 	gin.SetMode(gin.ReleaseMode) // Production ortamında gereksiz logları kapatmak için
 
-	// ! Gin ile Router Starting
+	//! Gin ile Router Starting
 	router := gin.Default() // Logger ve Recovery middleware'leri otomatik olarak ekler, tekrar eklemeye gerek kalmaz.
 
-	/* ! Eğer özel middleware'leri MANUEL eklemek isterseniz, aşağıdaki gibi yapabilirsiniz. Ancak gin.Default() zaten bu middleware'leri içerdiği için, tekrar eklemeye gerek yok.
-	r := gin.New()
-
-	r.Use(gin.Logger())
-	r.Use(gin.Recovery())
-	*/
-
-	// ? MIDDLEWARELAR
+	//! MIDDLEWARELAR
 	router.Use(middleware.AuthMiddleware(queries)) // Authentication middleware'ı tüm route'lara uygular. Her request'te API Key kontrolü yapar ve geçerli değilse 401 Unauthorized döner.
 	router.Use(middleware.RateLimitMiddleware())   // Rate Limiting middleware'ı tüm route'lara uygular. Her kullanıcı için belirli bir süre içinde kaç istek attığını takip eder ve limit aşılırsa 429 Too Many Requests döner.
+
+	// ? 2- HTTP-SERVER YAPILANDIRMASI
+	srv := http.Server{
+		Addr:    ":" + cfg.Port,
+		Handler: router,
+	}
+
+	// ? 3- Sunucuyu bir goroutine içerisinde başlatmak için.
+	go func() {
+		log.Printf("Gateway is running on : %s", cfg.Port)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s", err)
+		}
+	}()
+
+	// ? 4- KAPATMA SINYALLERI (SIGINT, SIGTERM)
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit // Sinyal gelene kadar bekler....
+	log.Println("Shutdown Server..")
+
+	// ? 5- Graceful Shutdown & İşletim sistemine kapanış için 5-10 saniye süre
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// ? 6- Yeni HTTP Requestleri Durdur..
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: ", err)
+	}
+
+	// ? 7- AnalyticsService'i kapat (Queue'de ki verilerin db'ye yazılmalarını bekler.)
+	analyticsService.Shutdown()
+
+	log.Println("server existing")
 
 	// ! TEST Endpoint
 	router.GET("/health", func(c *gin.Context) {
